1386cc9911f841e06f568e640c58972859a43b37
Alireza Ranjbar <azerila.rb@gmail.com>
2026-03-01 18:30:08 +0100

    feat(models): production schema

---
+++ app/models/production.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import List
+
+from pydantic import BaseModel
+
+
+class ProductionResults(BaseModel):
+    workOrderName: str
+    workOrderState: str
+    productId: str
+    productName: str
+    productRevision: str
+    traceabilityType: str
+    producedMtus: List[str]
+    uom: str
+    producedQuantity: int
+    scrappedQuantity: int
+    actualStartTime: datetime
+    actualEndTime: datetime
+    locationName: str
+    timestamp: datetime
+
